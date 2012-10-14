8a6a6e92bf304cb2ebd1c26b9f97fe0e49847872
Milind Parikh <milindparikh@gmail.com>
2012-10-13 22:15:24 -0700

    second commit

---
+++ src/erlkafka.hrl
@@ -0,0 +1,9 @@
+%%%-------------------------------------------------------------------
+%%% File     : erlkafka.hrl
+%%% Author   : Milind Parikh <milindparikh@gmail.com>
+%%%-------------------------------------------------------------------
+-author("Milind Parikh <milindparikh@gmail.com> [http://www.milindparikh.com]").
+
+-ifndef(erlkafka).
+-define(MAX_MSG_SIZE, 1048576).
+-endif.
